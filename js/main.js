bfa91a6a72a07e05478c4a04ff4a06a3d7e23aea
MUHAMMET GAZEL <112321778+MUHAMMETGAZEL@users.noreply.github.com>
2026-01-06 01:38:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -188,10 +188,12 @@ function setRotationVisible(isVisible) {
 
 async function fetchUserRole() {
   try {
+    const baseUrl = (location.hostname === 'localhost')
+      ? 'http://localhost:5001/api'
+      : 'https://startup-syria-backend.onrender.com/api';
+    
     const response = await fetch(
-      (location.hostname === 'localhost'
-        ? 'http://localhost:5001/api'
-        : 'https://api.aramlab.info/api') + '/auth/verify',
+      baseUrl + '/auth/verify',
       { credentials: 'include' }
     );
     if (!response.ok) return null;
